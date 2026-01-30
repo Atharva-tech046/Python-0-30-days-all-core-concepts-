681d8bf6e899011ecee55ad69a08a300ad503be7
Atharva Mohite <atharvam2017@gmail.com>
2026-01-30 16:45:18 +0530

    Add files via upload

---
+++ CHP01-variables.py
@@ -0,0 +1,25 @@
+# Variables = It is name given to a memory location in a program .... examples are given below 
+
+name =  'Atharva' #so here the variable is name and the data stored in the variable is 'Atharva'
+age = 20 
+weight = '78 kilos'
+
+# Now lets print variables 
+# So first we write the print statement and thn the name of the variable 
+# no need to write the variable name in quotes as we have already defines the variable above !
+
+print('My name is :',name)
+print('My age is :',age)
+print('My weight is:',weight)
+
+# now making a new variable age2 ..... saying age2=age 
+
+age2=age
+
+#now if we say print age2 it will print the same value decared in variable age since age2=age 
+
+print(age2)
+
+
+
+
